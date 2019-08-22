27e4c62a1a18388917611471da4ea17a1ad2fd91
himanshu1303 <himalaya.singh_cs18@gla.ac.in>
2019-08-22 15:33:29 +0530

    final commit done

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine().trim();
-        System.out.println("welcome," + username + "!")
+        System.out.println("welcome," + username + "!");
+        System.out.println("ho gaya");
         scanner.close();
     }
 
